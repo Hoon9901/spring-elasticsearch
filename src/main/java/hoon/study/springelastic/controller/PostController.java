802b1ab5e5ab2d4d4f77f50057cbb15d88257b2e
Hoon9901 <shonn.dev@gmail.com>
2023-11-22 22:34:12 +0900

    feat: Post 생성 및 검색 API 추가

--- src/main/java/hoon/study/springelastic/controller/PostController.java
+++ src/main/java/hoon/study/springelastic/controller/PostController.java
@@ -1,26 +1,40 @@
 package hoon.study.springelastic.controller;
 
+import hoon.study.springelastic.entity.PostDocument;
 import hoon.study.springelastic.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/post")
 public class PostController {
-
     private final PostService postService;
+
+    @Autowired
     public PostController(PostService postService) {
         this.postService = postService;
     }
 
     @PostMapping
-    public ResponseEntity createPost() {
+    public ResponseEntity createPost(
+            @RequestParam(value = "title") String title,
+            @RequestParam(value = "content") String content
+    ) {
 
-        postService.createPost();
+        postService.createPost(title, content);
 
         return new ResponseEntity("", HttpStatus.OK);
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity searchPost(
+            @RequestParam(value = "keyword") String keyword) {
+        List<PostDocument> posts = postService.searchPost(keyword);
+
+        return new ResponseEntity(posts, HttpStatus.OK);
     }
 }
--- src/main/java/hoon/study/springelastic/service/PostService.java
+++ src/main/java/hoon/study/springelastic/service/PostService.java
@@ -1,18 +1,41 @@
 package hoon.study.springelastic.service;
 
+import hoon.study.springelastic.entity.Post;
+import hoon.study.springelastic.entity.PostDocument;
+import hoon.study.springelastic.repository.PostDocumentRepository;
 import hoon.study.springelastic.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class PostService {
 
     private final PostRepository postRepository;
 
-    public PostService(PostRepository postRepository) {
+    private final PostDocumentRepository postDocumentRepository;
+
+    @Autowired
+    public PostService(PostRepository postRepository, PostDocumentRepository postDocumentRepository) {
         this.postRepository = postRepository;
+        this.postDocumentRepository = postDocumentRepository;
     }
 
-    public void createPost() {
-        throw new RuntimeException();
+    public void createPost(String title, String content) {
+        Post post = Post.builder()
+                .title(title)
+                .content(content)
+                .createdAt(LocalDateTime.now())
+                .build();
+
+        postRepository.save(post);
+        postDocumentRepository.save(PostDocument.from(post));
+
+    }
+
+    public List<PostDocument> searchPost(String keyword) {
+        return postDocumentRepository.findByTitleOrContent(keyword, keyword);
     }
 }
