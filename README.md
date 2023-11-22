ae7246666cb3c82bb592a3a3d0b77f64b35dba1d
Hoon9901 <shonn.dev@gmail.com>
2023-11-22 20:30:02 +0900

    feat: ElasticSearchConfig 구성 설정 추가

--- README.md
+++ README.md
@@ -14,6 +14,41 @@ Spring Boot 3.1.X + ElasticSearch 8.11.X
 - H2 Database
 - Lombok
 
+## 0. application.yaml 생성
+```yaml
+
+elasticsearch:
+  host: ${ELASTICSEARCH_HOST:localhost}
+  username: ${ELASTICSEARCH_USERNAME:elastic}
+  password: ${ELASTICSEARCH_PASSWORD:changeme}
+
+spring:
+  h2:
+    console:
+      enabled: true
+      path: /h2-console
+
+  datasource:
+    url: jdbc:h2:mem:testdb
+    username: sa
+    password:
+    driver-class-name: org.h2.Driver
+
+  jpa:
+    database-platform: org.hibernate.dialect.H2Dialect
+    hibernate:
+      ddl-auto: create-drop
+    show-sql: true
+    format-sql: true
+
+logging:
+  level:
+    org.hibernate.type.descriptor.sql: trace
+```
+
+elastic search host, username, password는 환경변수로 설정할 수 있도록 환경을 설정하거나 
+application.yaml 파일에 직접 설정하면 됩니다.
+
 ## 1. ElasticSearch Client Configuration
 
 ```java
---
+++ src/main/java/hoon/study/springelastic/config/ElasticSearchConfig.java
@@ -0,0 +1,28 @@
+package hoon.study.springelastic.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+@EnableElasticsearchRepositories
+public class ElasticSearchConfig extends ElasticsearchConfiguration {
+
+    @Value("${elasticsearch.host}")
+    private String host;
+
+    @Value("${elasticsearch.username}")
+    private String username;
+
+    @Value("${elasticsearch.password}")
+    private String password;
+
+    @Override
+    public ClientConfiguration clientConfiguration() {
+        return ClientConfiguration.builder()
+                .connectedTo(host)
+                .usingSsl()
+                .withBasicAuth(username, password)
+                .build();
+    }
+}
