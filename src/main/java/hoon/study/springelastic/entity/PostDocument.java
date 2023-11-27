60bcd004a5efd9027daf667bdacabd2157296f4f
Hoon9901 <shonn.dev@gmail.com>
2023-11-27 21:11:33 +0900

    feat: PostDocument Search API 로직 변경
    - 한글 검색을 위한 Setting 추가에 따른 Analyzer.json 생성
    - ElasticOperation을 이용한 쿼리빌더 및 검색 호출

--- src/main/java/hoon/study/springelastic/entity/PostDocument.java
+++ src/main/java/hoon/study/springelastic/entity/PostDocument.java
@@ -2,14 +2,12 @@ package hoon.study.springelastic.entity;
 
 import jakarta.persistence.Id;
 import lombok.*;
-import org.springframework.data.elasticsearch.annotations.DateFormat;
-import org.springframework.data.elasticsearch.annotations.Document;
-import org.springframework.data.elasticsearch.annotations.Field;
-import org.springframework.data.elasticsearch.annotations.FieldType;
+import org.springframework.data.elasticsearch.annotations.*;
 
 import java.time.LocalDateTime;
 
-@Document(indexName = "artscope-test-post")
+@Document(indexName = "artscope-test-post2")
+@Setting(settingPath = "es-config/es-analyzer.json")
 @Getter
 @Setter
 @NoArgsConstructor
@@ -21,7 +19,7 @@ public class PostDocument {
     @Field(type = FieldType.Long)
     private Long id;
 
-    @Field(type = FieldType.Text)
+    @Field(type = FieldType.Text, analyzer = "my_nori_analyzer", searchAnalyzer = "my_nori_analyzer")
     private String content;
 
     @Field(type = FieldType.Date, format = DateFormat.date_hour_minute_second)
--- src/main/java/hoon/study/springelastic/service/PostService.java
+++ src/main/java/hoon/study/springelastic/service/PostService.java
@@ -1,26 +1,34 @@
 package hoon.study.springelastic.service;
 
+import co.elastic.clients.elasticsearch._types.query_dsl.Query;
+import co.elastic.clients.elasticsearch._types.query_dsl.QueryBuilders;
 import hoon.study.springelastic.entity.Post;
 import hoon.study.springelastic.entity.PostDocument;
 import hoon.study.springelastic.repository.PostDocumentRepository;
 import hoon.study.springelastic.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.elasticsearch.client.elc.NativeQuery;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.SearchHit;
+import org.springframework.data.elasticsearch.core.SearchHits;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PostService {
 
     private final PostRepository postRepository;
-
     private final PostDocumentRepository postDocumentRepository;
+    private final ElasticsearchOperations elasticsearchOperations;
 
     @Autowired
-    public PostService(PostRepository postRepository, PostDocumentRepository postDocumentRepository) {
+    public PostService(PostRepository postRepository, PostDocumentRepository postDocumentRepository, ElasticsearchOperations elasticsearchOperations) {
         this.postRepository = postRepository;
         this.postDocumentRepository = postDocumentRepository;
+        this.elasticsearchOperations = elasticsearchOperations;
     }
 
     public void createPost(String title, String content) {
@@ -36,6 +44,12 @@ public class PostService {
     }
 
     public List<PostDocument> searchPost(String keyword) {
-        return postDocumentRepository.findByContent(keyword);
+        Query query = QueryBuilders.match(queryBuilder -> queryBuilder.field("content").query(keyword));
+        NativeQuery nativeQuery = NativeQuery.builder().withQuery(query).build();
+        SearchHits<PostDocument> result = elasticsearchOperations.search(nativeQuery, PostDocument.class);
+        return result
+                .stream()
+                .map(SearchHit::getContent)
+                .collect(Collectors.toList());
     }
 }
